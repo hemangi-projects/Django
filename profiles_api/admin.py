8867d759cd35e781a995afe5a247cd275bea00d5
Hemangi <hemangi@linescripts.com>
2022-02-09 23:12:56 +0530

    enabale django admin for user profile model

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from profiles_api import models
 
+
+admin.site.register(models.UserProfile)
+#this tells django admin to register our user profile model with the admin site means accessible through the admin interface
 # Register your models here.
