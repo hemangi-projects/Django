694c311dd60306de25bff36a8f82fff6f77cbdda
Hemangi <hemangi@linescripts.com>
2022-02-10 23:04:24 +0530

    Add serializer and put delete patch methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API view"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, formate=None):
         """Returns a list of APIView features"""
@@ -15,3 +18,31 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello!', 'an_apiview':an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handle updating an object
+            Put method will replace the object with provided object
+            e.g if there is 2 fields firstname and lastname and we give only lastname then it will remove firstname completely and change lastname field value.
+        """
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object
+            where patch method will only change the value which fields are given in request.
+            e.g if there is 2 fields firstname and lastname and we give only lastname then it will change only lastname and not change firstname field value.
+        """
+        return Response({'method':'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
